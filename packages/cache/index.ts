b0b476efc7b76c17bb74ccba8df2608f108daf68
estrattonbailey <ericstrattonbailey@gmail.com>
2020-08-20 10:42:57 -0500

    fix save, add debugging

--- packages/cache/index.ts
+++ packages/cache/index.ts
@@ -1,6 +1,8 @@
 import path from 'path'
 import ms from 'ms'
 import flatCache from 'flat-cache'
+import createDebug from 'debug'
+import assert from 'assert'
 
 type Loader = () => Promise<any>
 type Options = {
@@ -12,11 +14,19 @@ type Entry = {
   expires: number;
 }
 
+const debug = createDebug('presta-cache')
 const cwd = process.cwd()
+const dir = path.resolve(cwd, './.presta/cache')
 const c = flatCache.load('presta', path.resolve(cwd, './.presta/cache'));
+
+debug(`@presta/cache at ${dir}`)
 
 export async function cache(loader: Loader, options: Options) {
   const { key, duration } = options
+
+  assert(!!key, '@presta/cache expects a key')
+  assert(!!duration, '@presta/cache expects a duration')
+
   const interval = ms(duration)
 
   const entry: Entry = c.getKey(key)
@@ -26,8 +36,10 @@ export async function cache(loader: Loader, options: Options) {
     const { expires } = entry
 
     if (now > expires) {
+      debug(`${key} has expired`)
       c.removeKey(key)
     } else {
+      debug(`${key} is cached`)
       return entry.value
     }
   }
@@ -38,6 +50,10 @@ export async function cache(loader: Loader, options: Options) {
     value,
     expires: now + interval,
   })
+
+  c.save(true)
+
+  debug(`${key} has been cached for ${duration}`)
 
   return value
 }
