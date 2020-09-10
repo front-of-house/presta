ac7da0b01db217f593905b175108d5f122f015c3
estrattonbailey <ericstrattonbailey@gmail.com>
2020-09-09 22:43:45 -0500

    fix tests, fix bad check

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ const test = require('baretest')('presta')
 const assert = require('assert')
 
 require('./__test__/load.test')(test, assert)
-// require('./lib/__test__/createConfigFromCLI.test')(test, assert)
+require('./lib/__test__/createConfigFromCLI.test')(test, assert)
 require('./lib/__test__/createHeadTags.test')(test, assert)
 
 !(async function() {
