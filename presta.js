83535ea927a83a4f3c880885eaac3558e18fa9a5
Eric Bailey <ericstrattonbailey@gmail.com>
2020-10-06 10:31:18 -0500

    cursory typescript support (#1)
    * WIP ts
    
    * fix pragma, config extend, add extensions by default
    
    * reorder extensions by precedence
    
    * allow to auto-resolve extensions

--- presta.js
+++ presta.js
@@ -2,19 +2,31 @@
 
 require = require('esm')(module)
 
-const [_, jsx] = process.argv.join(' ').match(/--jsx[\s|=]([^-]+)/) || []
+const path = require('path')
 
-require('@babel/register')({
-  presets: [
-    [
-      '@babel/preset-react',
-      {
-        pragma: jsx || 'h',
-        pragmaFrag: jsx || 'h'
-      }
-    ]
-  ]
-})
+const [_, jsx = 'h'] = process.argv.join(' ').match(/--jsx[\s|=]([^-]+)/) || []
+
+const pragma = {
+  h: {
+    pragma: 'h',
+    pragmaFrag: 'h'
+  },
+  react: {
+    pragma: 'React.createElement',
+    pragmaFrag: 'React.Fragment'
+  }
+}
+
+const babel = {
+  extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  presets: [['@babel/preset-react', pragma[jsx] || pragma.h]]
+}
+
+try {
+  babel.extends = require.resolve(path.join(process.cwd(), 'babel.config.js'))
+} catch (e) {}
+
+require('@babel/register')(babel)
 
 require('module-alias').addAliases({
   '@': process.cwd()
