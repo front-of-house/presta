8e68122d0d1caecf5c5c0f700a2a998ae42084b2
estrattonbailey <ericstrattonbailey@gmail.com>
2020-10-05 21:49:56 -0500

    WIP ts

--- presta.js
+++ presta.js
@@ -2,13 +2,24 @@
 
 require = require('esm')(module)
 
+const path = require('path')
+
 const [_, jsx] = process.argv.join(' ').match(/--jsx[\s|=]([^-]+)/) || []
 
+const jsxPragmas = {
+  react: {
+    pragma: 'React.createElement',
+    pragmaFrag: 'React.Fragment'
+  }
+}
+
 require('@babel/register')({
+  extends: path.join(process.cwd(), 'babel.config.js'),
+  extensions: ['.js', '.jsx', '.ts', '.tsx'],
   presets: [
     [
       '@babel/preset-react',
-      {
+      jsxPragmas[jsx] || {
         pragma: jsx || 'h',
         pragmaFrag: jsx || 'h'
       }
