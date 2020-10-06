4a8c2fd5e1d1f73546cc9505e964d9cc53975be2
estrattonbailey <ericstrattonbailey@gmail.com>
2020-10-06 09:07:04 -0500

    fix pragma, config extend, add extensions by default

--- presta.js
+++ presta.js
@@ -4,28 +4,29 @@ require = require('esm')(module)
 
 const path = require('path')
 
-const [_, jsx] = process.argv.join(' ').match(/--jsx[\s|=]([^-]+)/) || []
+const [_, jsx = 'h'] = process.argv.join(' ').match(/--jsx[\s|=]([^-]+)/) || []
 
-const jsxPragmas = {
+const pragma = {
+  h: {
+    pragma: 'h',
+    pragmaFrag: 'h'
+  },
   react: {
     pragma: 'React.createElement',
     pragmaFrag: 'React.Fragment'
   }
 }
 
-require('@babel/register')({
-  extends: path.join(process.cwd(), 'babel.config.js'),
-  extensions: ['.js', '.jsx', '.ts', '.tsx'],
-  presets: [
-    [
-      '@babel/preset-react',
-      jsxPragmas[jsx] || {
-        pragma: jsx || 'h',
-        pragmaFrag: jsx || 'h'
-      }
-    ]
-  ]
-})
+const babel = {
+  extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  presets: [['@babel/preset-react', pragma[jsx] || pragma.h]]
+}
+
+try {
+  babel.extends = require.resolve(path.join(process.cwd(), 'babel.config.js'))
+} catch (e) {}
+
+require('@babel/register')(babel)
 
 require('module-alias').addAliases({
   '@': process.cwd()
