4183704dc9f106920ca9ab63f5650d868d51b003
estrattonbailey <ericstrattonbailey@gmail.com>
2020-11-11 18:52:35 -0600

    parse babel config automatically

--- presta.js
+++ presta.js
@@ -3,30 +3,24 @@
 require = require('esm')(module)
 
 const path = require('path')
+const { cosmiconfigSync } = require('cosmiconfig')
 
-const [_, jsx = 'h'] = process.argv.join(' ').match(/--jsx[\s|=]([^-]+)/) || []
+const { config: userBabelConfig } = cosmiconfigSync('babel').search() || {}
 
-const pragma = {
-  h: {
-    pragma: 'h',
-    pragmaFrag: 'h'
-  },
-  react: {
-    pragma: 'React.createElement',
-    pragmaFrag: 'React.Fragment'
+require('@babel/register')(
+  userBabelConfig || {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    presets: [
+      [
+        '@babel/preset-react',
+        {
+          pragma: 'h',
+          pragmaFrag: 'h'
+        }
+      ]
+    ]
   }
-}
-
-const babel = {
-  extensions: ['.js', '.jsx', '.ts', '.tsx'],
-  presets: [['@babel/preset-react', pragma[jsx] || pragma.h]]
-}
-
-try {
-  babel.extends = require.resolve(path.join(process.cwd(), 'babel.config.js'))
-} catch (e) {}
-
-require('@babel/register')(babel)
+)
 
 require('module-alias').addAliases({
   '@': process.cwd()
