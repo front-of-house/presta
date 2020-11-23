e32beaa0f6c46aa026b5359c829ac12b9cb042c1
estrattonbailey <ericstrattonbailey@gmail.com>
2020-11-23 17:10:30 -0600

    re-org handler, re-use babel

--- presta.js
+++ presta.js
@@ -4,23 +4,11 @@ require = require('esm')(module)
 
 const path = require('path')
 const { cosmiconfigSync } = require('cosmiconfig')
+const { config: defaultBabelConfig } = require('./lib/babel')
 
 const { config: userBabelConfig } = cosmiconfigSync('babel').search() || {}
 
-require('@babel/register')(
-  userBabelConfig || {
-    extensions: ['.js', '.jsx', '.ts', '.tsx'],
-    presets: [
-      [
-        '@babel/preset-react',
-        {
-          pragma: 'h',
-          pragmaFrag: 'h'
-        }
-      ]
-    ]
-  }
-)
+require('@babel/register')(userBabelConfig || defaultBabelConfig)
 
 require('module-alias').addAliases({
   '@': process.cwd()
