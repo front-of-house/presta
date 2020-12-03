512472bbde073fc3c6e930c3ba71f149f5e7e69f
estrattonbailey <ericstrattonbailey@gmail.com>
2020-12-02 20:32:28 -0600

    Revert "update 'route' to 'path' everywhere"
    This reverts commit 87c0b242064cd9a320edfa52bbacef997fdd9959.

--- router.js
+++ router.js
@@ -2,12 +2,12 @@ import rsort from 'route-sort'
 import toRegExp from 'regexparam'
 
 export function createRouter (pages, userConfig) {
-  const routes = pages.map(p => p.path)
+  const routes = pages.map(p => p.route)
   const sortedRoutes = rsort(routes)
   const sortedPages = sortedRoutes.map(r => {
-    return pages.find(p => p.path === r)
+    return pages.find(p => p.route === r)
   })
-  const preparedRoutes = sortedPages.map(p => [toRegExp(p.path), p])
+  const preparedRoutes = sortedPages.map(p => [toRegExp(p.route), p])
 
   return url => {
     for (const [{ pattern }, page] of preparedRoutes) {
