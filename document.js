faab9ddf6d0db81d1fc5de930a8f373fd037fd53
estrattonbailey <ericstrattonbailey@gmail.com>
2020-12-04 16:02:00 -0600

    fix doc test

--- document.js
+++ document.js
@@ -7,21 +7,23 @@ export function document ({
   htmlAttributes = {},
   bodyAttributes = {}
 }) {
-  if (!head.link.find(m => m.rel === 'icon')) {
+  if (head.link && !head.link.find(m => m.rel === 'icon')) {
     head.link.push({
       rel: 'icon',
       type: 'image/svg+xml',
       href: `data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16' style='color: white'><text x='0' y='14'>◊</text></svg>`
     })
   }
-  if (!head.meta.find(m => !!m.charset)) {
-    head.meta.push({ charset: 'UTF-8' })
-  }
-  if (!head.meta.find(m => m.name === 'viewport')) {
-    head.meta.push({
-      name: 'viewport',
-      content: 'width=device-width,initial-scale=1'
-    })
+  if (head.meta) {
+    if (!head.meta.find(m => !!m.charset)) {
+      head.meta.push({ charset: 'UTF-8' })
+    }
+    if (!head.meta.find(m => m.name === 'viewport')) {
+      head.meta.push({
+        name: 'viewport',
+        content: 'width=device-width,initial-scale=1'
+      })
+    }
   }
 
   const headTags = createHeadTags(head)
