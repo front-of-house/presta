5f60c8f9c42fd0e40c7393fbdd810938bd788210
estrattonbailey <ericstrattonbailey@gmail.com>
2020-12-11 11:09:48 -0600

    docs: fix button color

--- docs/src/components/Button.js
+++ docs/src/components/Button.js
@@ -7,11 +7,11 @@ export function Button ({ children, ...props }) {
       as='button'
       py={2}
       px={4}
-      c='b'
       bg='pink'
       fs={6}
       fe={4}
       css={{
+        color: 'var(--dark) !important',
         border: '2px solid currentColor',
         borderRadius: '6px',
         textDecoration: 'none'
