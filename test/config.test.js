75f4195e6da15d568f8f8491ee46627e89c06669
estrattonbailey <ericstrattonbailey@gmail.com>
2020-12-27 12:36:28 -0600

    fix(config): resolve configfile path instead of join

--- test/config.test.js
+++ test/config.test.js
@@ -3,57 +3,62 @@ import path from 'path'
 import * as fixtures from './fixtures'
 import { create, unmerge } from '../lib/config'
 
-const pages = 'app/**/*.js'
-const output = 'output'
-const assets = 'assets'
-const c = 'presta.config.js'
-
 export default async function (test, assert) {
   test('config - defaults', async () => {
-    const config = create({
-      pages
-    })
+    const pages = 'app/**/*.js'
+    const cli = { pages }
+    const config = create(cli)
+
+    assert.deepEqual(config._cli, cli)
+    assert.deepEqual(config._config, {})
 
     assert(config.pages.includes(pages))
     assert(path.isAbsolute(config.output))
-    assert(config.cwd === fixtures.getRoot())
+    assert(path.isAbsolute(config.assets))
+    assert(path.isAbsolute(config.cwd))
+    assert(config.configFilepath === undefined)
+  })
+
+  test('config - no pages', async () => {
+    const cli = {}
+    const config = create(cli)
+
+    assert.deepEqual(config.pages, [])
   })
 
   test('config - output', async () => {
-    const config = create({
-      pages,
-      output
-    })
+    const pages = 'app/**/*.js'
+    const output = 'dist'
+    const cli = { pages, output }
+    const config = create(cli)
 
-    assert(/\/output/.test(config.output))
+    assert(config.output.includes(output))
     assert(path.isAbsolute(config.output))
   })
 
   test('config - assets', async () => {
-    const config = create({
-      pages,
-      output,
-      assets
-    })
+    const pages = 'app/**/*.js'
+    const output = 'dist'
+    const assets = 'assets'
+    const cli = { pages, output, assets }
+    const config = create(cli)
 
-    assert(/\/assets/.test(config.assets))
+    assert(config.assets.includes(assets))
     assert(path.isAbsolute(config.assets))
   })
 
   test('config - picks up default file if present', async () => {
     const pages = './pages/*.js'
     const output = 'output'
-    const files = {
+    const fsx = fixtures.create({
       config: {
         url: 'presta.config.js',
         content: `export const pages = '${pages}'; export const output = '${output}'`
       }
-    }
-    const fsx = fixtures.create(files)
+    })
 
     const config = create({})
 
-    assert(/presta\.config/.test(config.configFilepath))
     assert(path.isAbsolute(config.configFilepath))
     assert(config.pages.includes(pages))
     assert(config.output.includes(output))
@@ -64,19 +69,18 @@ export default async function (test, assert) {
   test('config - picks up custom file if present', async () => {
     const pages = './pages/*.js'
     const output = 'output'
-    const files = {
+    const fsx = fixtures.create({
       config: {
         url: 'presta-config.js',
         content: `export const pages = '${pages}'; export const output = '${output}'`
       }
-    }
-    const fsx = fixtures.create(files)
-
-    const config = create({
-      config: files.config.url
     })
 
-    assert(/presta-config/.test(config.configFilepath))
+    const config = create({
+      config: fsx.files.config
+    })
+
+    assert(config.configFilepath.includes(fsx.files.config))
     assert(path.isAbsolute(config.configFilepath))
     assert(config.pages.includes(pages))
     assert(config.output.includes(output))
@@ -85,13 +89,12 @@ export default async function (test, assert) {
   })
 
   test('config - overriden by CLI args', async () => {
-    const files = {
+    const fsx = fixtures.create({
       config: {
         url: 'presta.config.js',
         content: `export const pages = './pages/*.js'; export const output = './output'`
       }
-    }
-    const fsx = fixtures.create(files)
+    })
 
     const config = create({
       pages: 'foo',
@@ -108,18 +111,17 @@ export default async function (test, assert) {
   })
 
   test('config - file is merged with internal config', async () => {
-    const files = {
+    const fsx = fixtures.create({
       config: {
         url: 'presta.merged.config.js',
         content: `export function createContent(context) {}`
       }
-    }
-    const fsx = fixtures.create(files)
+    })
 
     const config = create({
       pages: 'foo',
       output: 'dist',
-      config: 'presta.merged.config.js'
+      config: fsx.files.config
     })
 
     assert(!!config.createContent)
@@ -128,7 +130,7 @@ export default async function (test, assert) {
   })
 
   test('config - unmerge', async () => {
-    const files = {
+    const fsx = fixtures.create({
       config: {
         url: 'presta.unmerged.config.js',
         content: `
@@ -137,8 +139,7 @@ export default async function (test, assert) {
           export function createContent(context) {}
         `
       }
-    }
-    const fsx = fixtures.create(files)
+    })
 
     const curr = create({
       pages: 'bar',
@@ -146,12 +147,13 @@ export default async function (test, assert) {
     })
     const prev = require(fsx.files.config)
 
-    assert(curr.pages.length === 2)
+    assert(curr.pages.length === 2) // merged
     assert(curr.output.includes('output'))
 
     const unmerged = unmerge(curr, prev)
 
     assert(unmerged.pages.length === 1)
+    assert(unmerged.pages[0] === 'bar')
     assert(unmerged.output.includes('build'))
 
     fsx.cleanup()
