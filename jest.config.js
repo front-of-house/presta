8d98b9dc40307a8838b9322f58754eca1b36f3e7
estrattonbailey <ericstrattonbailey@gmail.com>
2021-07-07 14:33:22 -0500

    chore: mv docs site

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   collectCoverage: true,
-  collectCoverageFrom: ['packages/**/*.{ts}', '!**/node_modules/**', '!**/docs/**', '!**/examples/**'],
-  modulePathIgnorePatterns: ['/node_modules/', 'docs', 'examples'],
+  collectCoverageFrom: ['packages/**/*.{ts}', '!**/node_modules/**', '!**/site/**', '!**/examples/**'],
+  modulePathIgnorePatterns: ['/node_modules/', 'site', 'examples'],
   testPathIgnorePatterns: ['/node_modules/', 'packages/presta'],
 }
