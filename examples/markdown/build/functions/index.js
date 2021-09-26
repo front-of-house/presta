2f5bcf212284f15c12650906978d3db34bcc8824
estrattonbailey <ericstrattonbailey@gmail.com>
2021-09-26 12:11:51 -0500

    chore: ignore built files in markdown example

--- examples/markdown/build/functions/index.js
+++
@@ -1,4 +0,0 @@
-import { wrapHandler } from 'presta';
-import * as file from '/Users/ericbailey/Sites/sure-thing/presta/examples/markdown/index.js';
-export const route = file.route
-export const handler = wrapHandler(file)
