4490d30eab71a77eb19b1288703636865a97ea45
estrattonbailey <ericstrattonbailey@gmail.com>
2021-11-28 12:26:40 -0600

    docs: update basic example

--- examples/basic/components/Nav.ts
+++ examples/basic/components/Nav.ts
@@ -5,6 +5,8 @@ export function Nav({ currentPath }: { currentPath?: string } = {}) {
         { href: '/', title: 'Home' },
         { href: '/about', title: 'About' },
         { href: '/contact', title: 'Contact' },
+        { href: '/some-page', title: 'Some Page' },
+        { href: '/not/found', title: '404' },
       ]
         .map(
           (link) => `<a href='${link.href}' class='mr4 ${currentPath === link.href ? 'active' : ''}'>${link.title}</a>`
