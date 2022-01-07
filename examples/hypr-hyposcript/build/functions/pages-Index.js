b20664a2bda8ed6318019cbbebd90b517201ec2a
estrattonbailey <ericstrattonbailey@gmail.com>
2022-01-06 19:04:55 -0600

    chore: update examples

--- examples/hypr-hyposcript/build/functions/pages-Index.js
+++
@@ -1,4 +0,0 @@
-import { wrapHandler } from 'presta';
-      import * as file from '/Users/ericbailey/Sites/sure-thing/presta/examples/hypr-hyposcript/pages/Index.tsx';
-      export const route = file.route
-      export const handler = wrapHandler(file)
