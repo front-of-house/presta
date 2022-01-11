f085d99c4b5b119fad102ee0513a9868b259e19d
estrattonbailey <ericstrattonbailey@gmail.com>
2022-01-10 19:32:31 -0600

    feat: vercel adapter

---
+++ .changeset/hungry-fishes-run.md
@@ -0,0 +1,5 @@
+---
+'@presta/adapter-vercel': minor
+---
+
+Initial release
---
+++ packages/adapter-vercel/__tests__/index.ts
@@ -0,0 +1,14 @@
+import fs from 'fs'
+import path from 'path'
+import { suite } from 'uvu'
+import * as assert from 'uvu/assert'
+import { afix } from 'afix'
+
+// import createPlugin, {
+// } from '../index'
+
+const test = suite('@presta/adapter-vercel')
+
+test('', async () => {})
+
+test.run()
